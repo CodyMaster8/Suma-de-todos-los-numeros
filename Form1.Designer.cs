1fb9e70f28cdca35bb3abda0b2f12fcfe3c8189f
Cody Master <CodyMaster80808@gmail.com>
2023-11-02 13:29:30 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Suma_de_todos_los_numeros
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            listBox1 = new ListBox();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            label3 = new Label();
+            textBox2 = new TextBox();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(209, 22);
+            label1.Name = "label1";
+            label1.Size = new Size(351, 21);
+            label1.TabIndex = 0;
+            label1.Text = "Este programa suma los numeros capturados";
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(676, 61);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(120, 259);
+            listBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(279, 96);
+            label2.Name = "label2";
+            label2.Size = new Size(172, 21);
+            label2.TabIndex = 2;
+            label2.Text = "Introduce un numero";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(457, 94);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(103, 23);
+            textBox1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(473, 123);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 4;
+            button1.Text = "Añadir";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(289, 239);
+            label3.Name = "label3";
+            label3.Size = new Size(162, 21);
+            label3.TabIndex = 5;
+            label3.Text = "La suma de todos es";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(457, 239);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 6;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(187, 221);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 39);
+            button2.TabIndex = 7;
+            button2.Text = "Sumar Numeros";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button2);
+            Controls.Add(textBox2);
+            Controls.Add(label3);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(listBox1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Suma de Numeros";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private ListBox listBox1;
+        private Label label2;
+        private TextBox textBox1;
+        private Button button1;
+        private Label label3;
+        private TextBox textBox2;
+        private Button button2;
+    }
+}
